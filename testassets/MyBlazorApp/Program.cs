66e861978bb0f25788153e119b384368aec8f0e7
ottodobretsberger <39414503+ottodobretsberger@users.noreply.github.com>
2020-12-07 13:09:27 -0600

    initial first commit

---
+++ testassets/MyBlazorApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using WebWindows.Blazor;
+using System;
+
+namespace MyBlazorApp
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ComponentsDesktop.Run<Startup>("My Blazor App", "wwwroot/index.html");
+        }
+    }
+}
